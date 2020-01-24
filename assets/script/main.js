4ee065b7d9bad115901122e9986306d0cdad7f31
vancerweston <vweston@student.neumont.edu>
2020-01-24 12:14:56 -0700

    created a main.js for a small assignment

---
+++ assets/script/main.js
@@ -0,0 +1,14 @@
+let x = 10;
+console.log(x);
+let y = 5;
+console.log(y);
+let addition = x + y;
+console.log(addition);
+let subtraction = x - y;
+console.log(subtraction);
+let multiply = x * y;
+console.log(multiply);
+let divition = x / y;
+console.log(divition);
+let remainder = x % y;
+console.log(remainder);
